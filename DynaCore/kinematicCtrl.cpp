c32dcc07fc4d8aab4135f7876990bc0ce55b64fa
Haoguang (John) Yang <yhg14@mails.tsinghua.edu.cn>
2017-01-31 17:42:19 +0800

    on feedback design

--- DynaCore/kinematicCtrl.cpp
+++ DynaCore/kinematicCtrl.cpp
@@ -9,8 +9,15 @@ typedef struct VehiclePhysicalParams {
 typedef struct SensorParams{
 	int encoderRes;
 	//bla bla bla...
-}
+};
+typedef struct Kinematic{
+	double speed[2];
+	double omega;
+	double accel[2];
+	double angularAccel;
+};
 VehiclePhysicalParams Vehicle;
+Kinematic Actual;
 
 /*void GetVehicleData(VehiclePhysicalParams Vehicle)
 {
@@ -47,19 +54,38 @@ void KinematicCentralizedSteering(double radius, double speed, double* steerVal,
 	{
 		double leftBase = radius-Vehicle.TrackWidth/2;
 		double rightBase = radius+Vehicle.TrackWidth/2;
-		steerVal[1] = atan(Vehicle.WheelBase/2/leftBase);
-		steerVal[2] = atan(Vehicle.WheelBase/2/rightBase);
+		steerVal[0] = atan(Vehicle.WheelBase/2/leftBase);
+		steerVal[1] = atan(Vehicle.WheelBase/2/rightBase);
+		steerVal[2] = -steerVal[0];
 		steerVal[3] = -steerVal[1];
-		steerVal[4] = -steerVal[4];
 		double omega = speed/radius;
 		double Rleft = sqrt(leftBase*leftBase+Vehicle.WheelBase*Vehicle.WheelBase/4);
 		double Rright = sqrt(rightBase*rightBase+Vehicle.WheelBase*Vehicle.WheelBase/4);
-		driveVal[1] = omega*Rleft;
-		driveVal[2] = omega*Rright;
+		driveVal[0] = omega*Rleft;
+		driveVal[1] = omega*Rright;
+		driveVal[2] = driveVal[0];
 		driveVal[3] = driveVal[1];
-		driveVal[4] = driveVal[2];
 	}
 	//steerVal NEEDS CONVERSION!!!
 	//driveVal NEEDS CONVERSION!!!
 	return;
+}
+
+int pidController(Kinematic Target, double* steerActual, double* driveActual, double* steerValCorrection, double* driveValCorrection)
+{
+	double u[4];
+	double v[4];
+	Kinematic Error;
+	for (int i=0;i<4;i++)
+	{
+		u[i] = driveActual[i]*cos(steerActual[i]);
+		v[i] = driveActual[i]*sin(steerActual[i]);
+	}
+	Actual.speed[0] = (u(1)+u(2)+u(3)+u(4))/4;
+	Actual.speed[1] = (v(1)+v(2)+v(3)+v(4))/4;
+	Actual.omega = ((u[2]-u[1]+u[4]-u[3])/Vehicle.TrackWidth+(v[1]-v[3]+v[2]-v[4])/Vehicle.WheelBase)/4;
+	Error.speed = Actual.speed - Target.speed;
+	Error.omega = Actual.omega - Target.omega;
+	//DESIGN FEEDBACKS HERE...
+	return 0;
 }
