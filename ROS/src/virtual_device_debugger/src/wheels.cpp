48e67c2f4a9e9325a218e3cdf8955b1d124800c8
Haoguang (John) Yang <yhg14@mails.tsinghua.edu.cn>
2017-05-06 22:28:38 +0800

    passed compilation of virtual vehicle

--- ROS/src/virtual_device_debugger/src/wheels.cpp
+++ ROS/src/virtual_device_debugger/src/wheels.cpp
@@ -1,37 +1,26 @@
 #include <ros/ros.h>
-//#include <FlexiTimer2.h>
+#include <sys/time.h>
 #include <std_msgs/UInt16MultiArray.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int32MultiArray.h>
-
-/*
-#define CONTRL      3
-#define BACK        4
-#define BREAK       5 //connecting to driving motor controller.
-#define csn        13 //yellow
-#define datS        2 //green
-#define datD        8 //green
-#define clk         9 //blue
-#define PUL         6
-#define DIR         7 // direction for the angle motor
-#define VOLT        A3
-#define AMPD        A7
-#define AMPS        A6
-*/
+#include <math.h>
+using namespace std;
 
 ros::NodeHandle handle;
 const uint16_t encoder_resolution = 4096;
 const unsigned int updateTime = 40000;	                    //Time in us
-int pulseTime[4] = {100, 100, 100, 100};                	//Time in us
+const unsigned int queryTime = 1000;
+unsigned int pulseTime[4] = {100, 100, 100, 100};                	//Time in us
 uint16_t Angle[4] = {0,0,0,0};
 float angle_real[4]={0., 0., 0., 0.};
-int16_t Speed[4] = {0,0,0,0};
+//int16_t Speed[4] = {0,0,0,0};
 uint16_t steeringTarget[4] = {0,0,0,0};
 uint16_t _zero[4] = {231,2646,1895,2297};
 uint16_t _last[4] = {0,0,0,0};
 uint16_t drive_input[4];
 uint16_t drive_val[4];
 float vel[4]={0.,0.,0.,0.};
+float throttle[4] = {1.0, 1.0, 1.0, 1.0};
 
 std_msgs::UInt16MultiArray ActuatorStatus[4];
 /************
@@ -63,19 +52,44 @@ ros::Publisher assessPower[4] = {handle.advertise<std_msgs::Float32MultiArray>("
                                  handle.advertise<std_msgs::Float32MultiArray>("UnitPower02", 2), \
                                  handle.advertise<std_msgs::Float32MultiArray>("UnitPower03", 2)};
 
-unsigned long time_last;                 //for Buffer flushing
-unsigned long time_last_query;           //for Query
+struct timeval time_last[4];              //for Buffer flushing
+struct timeval time_last_query;           //for Query
+struct timeval time_last_publish;
 
+void setup() {
+	
+	for (int i=0; i<4; i++){
+    PowerStatus[i].layout.dim.push_back(std_msgs::MultiArrayDimension());
+    PowerStatus[i].layout.dim[0].label = "UnitPower";
+    PowerStatus[i].layout.dim[0].size = 3;
+    PowerStatus[i].layout.dim[0].stride = 1*3;
+    for (int j = 0; j < 3; j++){
+        PowerStatus[i].data.push_back(0.0);
+        }
+    
+    
+    ActuatorStatus[i].layout.dim.push_back(std_msgs::MultiArrayDimension());
+    ActuatorStatus[i].layout.dim[0].label = "WheelActual";
+    ActuatorStatus[i].layout.dim[0].size = 2;
+    ActuatorStatus[i].layout.dim[0].stride = 1*2;
+    for (int j = 0; j < 2; j++){
+        PowerStatus[i].data.push_back(0);
+        }
+    
+    steeringTarget[i] = ActuatorStatus[i].data[0];	//Stop Init Steering of the wheel
+    }
+	
+}
 
 void Actuate0( const std_msgs::UInt16MultiArray& ctrl_var){
     int i=0;
     //Send Signals to stepper motor and BLDC according to messages subscribed
     drive_input[i] = ctrl_var.data[1];
 	if(ctrl_var.data[3]>0){ // 倒车
-        vel[i] = vel[i] - drive_input[i];
+        vel[i] = vel[i] - drive_input[i]*throttle[i];
 	}//adjust for the switch
     else{
-		vel[i] = vel[i] + drive_input[i];
+		vel[i] = vel[i] + drive_input[i]*throttle[i];
 	}
 	if (ctrl_var.data[2]==0){
 	    vel[i] = vel[i]*0.98;
@@ -92,10 +106,10 @@ void Actuate1( const std_msgs::UInt16MultiArray& ctrl_var){
     //Send Signals to stepper motor and BLDC according to messages subscribed
     drive_input[i] = ctrl_var.data[1];
 	if(ctrl_var.data[3]>0){ // 倒车
-        vel[i] = vel[i] - drive_input[i];
+        vel[i] = vel[i] - drive_input[i]*throttle[i];
 	}//adjust for the switch
     else{
-		vel[i] = vel[i] + drive_input[i];
+		vel[i] = vel[i] + drive_input[i]*throttle[i];
 	}
 	if (ctrl_var.data[2]==0){
 	    vel[i] = vel[i]*0.98;
@@ -112,10 +126,10 @@ void Actuate2( const std_msgs::UInt16MultiArray& ctrl_var){
     //Send Signals to stepper motor and BLDC according to messages subscribed
     drive_input[i] = ctrl_var.data[1];
 	if(ctrl_var.data[3]>0){ // 倒车
-        vel[i] = vel[i] - drive_input[i];
+        vel[i] = vel[i] - drive_input[i]*throttle[i];
 	}//adjust for the switch
     else{
-		vel[i] = vel[i] + drive_input[i];
+		vel[i] = vel[i] + drive_input[i]*throttle[i];
 	}
 	if (ctrl_var.data[2]==0){
 	    vel[i] = vel[i]*0.98;
@@ -132,10 +146,10 @@ void Actuate3( const std_msgs::UInt16MultiArray& ctrl_var){
     //Send Signals to stepper motor and BLDC according to messages subscribed
     drive_input[i] = ctrl_var.data[1];
 	if(ctrl_var.data[3]>0){ // 倒车
-        vel[i] = vel[i] - drive_input[i];
+        vel[i] = vel[i] - drive_input[i]*throttle[i];
 	}//adjust for the switch
     else{
-		vel[i] = vel[i] + drive_input[i];
+		vel[i] = vel[i] + drive_input[i]*throttle[i];
 	}
 	if (ctrl_var.data[2]==0){
 	    vel[i] = vel[i]*0.98;
@@ -164,8 +178,7 @@ void Flip(bool direc, uint8_t which_one) {
   }
 }
 
-void Steering(){
-for (int i=0; i<4; i++){
+void Steering(int i){
 	//-------------------start angle control------------------------------------
 	if(steeringTarget[i] < 0 || steeringTarget[i] >encoder_resolution-1) { // will be modified as -90 degree to 90 degree
         //Serial.println("bad DesiredAngle input.");
@@ -184,112 +197,67 @@ for (int i=0; i<4; i++){
         }
         //end while loop 
     }
-    }
 	//----------------end angle control---------------------------------------------  
 }
 
-void setup() {
-	
-	for (int i=0; i<4; i++){
-    //PowerStatus.layout.dim_length = 1;
-    //malloc(sizeof(std_msgs::MultiArrayDimension) * 3);
-    PowerStatus[i].layout.dim.push_back(std_msgs::MultiArrayDimension());
-    PowerStatus[i].layout.dim[0].label = "UnitPower";
-    PowerStatus[i].layout.dim[0].size = 3;
-    PowerStatus[i].layout.dim[0].stride = 1*3;
-    //PowerStatus.layout.data_offset = 0;
-    //PowerStatus[i].data = (float *)malloc(sizeof(float)*3);
-    //PowerStatus[i].data_length = 3;
-    for (int j = 0; j < 3; j++){
-        PowerStatus[i].data.push_back(0.0);
-        }
-    
-    //ActuatorStatus.layout.dim_length = 1;
-    //malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
-    ActuatorStatus[i].layout.dim.push_back(std_msgs::MultiArrayDimension());
-    ActuatorStatus[i].layout.dim[0].label = "WheelActual";
-    ActuatorStatus[i].layout.dim[0].size = 2;
-    ActuatorStatus[i].layout.dim[0].stride = 1*2;
-    //ActuatorStatus.layout.data_offset = 0;
-    //ActuatorStatus[i].data = (uint16_t *)malloc(sizeof(uint16_t)*2);
-    //ActuatorStatus[i].data_length = 2;
-    for (int j = 0; j < 2; j++){
-        PowerStatus[i].data.push_back(0);
-        }
-    
-    steeringTarget[i] = ActuatorStatus[i].data[0];	//Stop Init Steering of the wheel
-    }
-	
-	//Initiate Steering Actuator and run via FlexiTimer
-	//Query();
-	//FlexiTimer2::set(updateTime,Query);  //time in the unit of ms
-    //FlexiTimer2::start();
+void Throttling(int i){
+    throttle[i] = min(throttle[i] * 1050.0 / max(PowerStatus[i].data[0]*PowerStatus[i].data[2],(float)0.001),1.0);
 }
 
-void loop() {
-   unsigned long time_now = micros();
-   if ((unsigned long)(time_now - time_last) > pulseTime){
-       Steering();
-       time_last = micros();
-   }
-   if ((unsigned long)(time_now - time_last_query) > updateTime){
-       Query();
-       time_last_query = micros();
-   }
-   handle.spinOnce();
-}
+uint16_t dataActuator[4][2] = {0};
+float dataPower[4][3];
 
-void Query()
+void Query(int i)
 {
-  uint16_t dataActuator[2] = {0};
-  float dataPower[3];
+  dataActuator[i][0] = angle_real[i]/360*4096+_zero[i];
+  dataActuator[i][1] = dataActuator[i][1]+vel[i]/4096;
+  Angle[i]=(dataActuator[i][0]-_zero[i]+encoder_resolution)%encoder_resolution;
   
-  /*
-  digitalWrite(csn,LOW);
-  delayMicroseconds(1);
-  for (int k=0;k<12;k++)
-  {
-    digitalWrite(clk,LOW);
-    delayMicroseconds(1);
-    digitalWrite(clk,HIGH);
-    delayMicroseconds(1);
-    dataActuator[0]=(dataActuator[0]<<1)+digitalRead(datS);		//Steering
-    dataActuator[1]=(dataActuator[1]<<1)+digitalRead(datD);		//Driving
-    //delayMicroseconds(1);
-  }
-  for (int k=0;k<6;k++)
-  {
-    digitalWrite(clk,LOW);
-    delayMicroseconds(1);
-    digitalWrite(clk,HIGH);
-    delayMicroseconds(1);
-  }
-  digitalWrite(csn,HIGH);
-  */
-
-  dataActuator[0] = angle_real/360*4096+_zero;
-  dataActuator[1] = dataActuator[1]+vel/4096;
-  Angle=(dataActuator[0]-_zero+encoder_resolution)%encoder_resolution;
-  Speed=(-dataActuator[1]+_last+encoder_resolution)%encoder_resolution;
-  _last = dataActuator[1];
-  //Angle=dataActuator[0];
+  dataPower[i][0] = 50*(0.02892+0.00002576*50)+2.99;
+  dataPower[i][1] = 5.0;
+  dataPower[i][2] = drive_input[i]*vel[i]/62500;
   
-  dataPower[0] = 50*(0.02892+0.00002576*50)+2.99;
-  dataPower[1] = 5.0;
-  dataPower[2] = drive_input[0]*vel[0]/62500;
-  
-  ActuatorStatus.data = dataActuator;
-  PowerStatus.data = dataPower;
-  
-  assessActual.publish (&ActuatorStatus);
-  assessPower.publish (&PowerStatus);
   //Serial.write((const uint8_t*)&to_send,sizeof(serial_format));
 }
 
-void main(){
+void Publish(int i){
+  ActuatorStatus[i].data[0] = dataActuator[i][0];
+  ActuatorStatus[i].data[1] = dataActuator[i][1];
+  PowerStatus[i].data[0] = (float)dataPower[i][0];
+  PowerStatus[i].data[1] = (float)dataPower[i][1];
+  PowerStatus[i].data[2] = (float)dataPower[i][2];
+  
+  assessActual[i].publish (ActuatorStatus[i]);
+  assessPower[i].publish (PowerStatus[i]);
+}
+
+void loop() {
+   struct timeval time_now;
+   gettimeofday(&time_now, NULL);
+   for (int i=0; i<4; i++){
+   if ((unsigned long)(time_now.tv_usec - time_last[i].tv_usec) > pulseTime[i]){
+       Steering(i);
+       gettimeofday(&time_last[i], NULL); //time_last = micros();
+   }
+   if ((unsigned long)(time_now.tv_usec - time_last_query.tv_usec) > queryTime){
+       Query(i);
+       Throttling(i);
+       gettimeofday(&time_last_query, NULL); //time_last_query = micros();
+   }
+   if ((unsigned long)(time_now.tv_usec - time_last_publish.tv_usec) > updateTime){
+	   Publish(i);
+	   gettimeofday(&time_last_publish, NULL); //time_last_publish = micros();
+   }
+   }
+   ros::spinOnce();
+}
+
+int main(){
     setup();
     while (ros::ok()){
         loop();
+        ros::spinOnce();
     }
+    return 0;
 }
 
