e9ab6045acdac54f8246b9f3f4f0357d94a1df26
Haoguang (John) Yang <yhg14@mails.tsinghua.edu.cn>
2017-05-08 23:40:16 +0800

    updated some CMake files

--- ROS/devel/share/agile_v_vision/cmake/agile_v_visionConfig.cmake
+++ ROS/devel/share/agile_v_vision/cmake/agile_v_visionConfig.cmake
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(agile_v_vision_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT " " STREQUAL " ")
+if(NOT "/mnt/Data/AgileV_dev/ROS/src/agile_v_vision/include " STREQUAL " ")
   set(agile_v_vision_INCLUDE_DIRS "")
-  set(_include_dirs "")
+  set(_include_dirs "/mnt/Data/AgileV_dev/ROS/src/agile_v_vision/include")
   foreach(idir ${_include_dirs})
     if(IS_ABSOLUTE ${idir} AND IS_DIRECTORY ${idir})
       set(include ${idir})
@@ -109,7 +109,7 @@ if(NOT " " STREQUAL " ")
   endforeach()
 endif()
 
-set(libraries "")
+set(libraries "agile_v_vision")
 foreach(library ${libraries})
   # keep build configuration keywords, target names and absolute libraries as-is
   if("${library}" MATCHES "^(debug|optimized|general)$")
@@ -153,7 +153,7 @@ foreach(t ${agile_v_vision_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "")
+set(depends "cv_bridge;image_transport;roscpp;rospy;sensor_msgs;std_msgs")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
